131bba9bcb700821e2552823e0c409169e9d9f5c
Alym62 <111710522+Alym62@users.noreply.github.com>
2022-12-08 12:44:15 -0300

    Add files via upload

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+padding: 10px;
+border: 1px solid black;
+background-color: #00aad0;
+color: white;
+font-size: 24px;
+font-weight: 700;
+flex: 1;
+
+
+
+
+`
